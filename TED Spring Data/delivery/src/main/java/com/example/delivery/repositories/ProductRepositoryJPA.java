6b079f53196e20b8aac07dac66592b393303e5a9
Ewerton Alexander <91163104+Ewertonalex@users.noreply.github.com>
2023-03-21 09:32:42 -0300

    Add files via upload

---
+++ TED Spring Data/delivery/src/main/java/com/example/delivery/repositories/ProductRepositoryJPA.java
@@ -0,0 +1,23 @@
+package com.example.delivery.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.delivery.entities.Product;
+
+public interface ProductRepositoryJPA extends JpaRepository<Product, Long>{
+	
+	
+	@Query("SELECT p FROM Product p WHERE p.id = :id")
+	Product findByProduct(Integer id);
+	
+	
+	@Query(value="SELECT p.* FROM tb_product p  WHERE p.id = :id", nativeQuery = true)
+	Product findByProductnative(Integer id);
+
+	@Query("SELECT p FROM Product p")
+	List<Product> findAllProducts();
+
+	@Query(value = "SELECT * FROM Product", nativeQuery = true)
+	List<Product> findAllProductsSQL();
+}
